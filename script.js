9dcb0afa71603c856679021546020dac96bf7541
Ronny Håland <ronnyhaland@Ronnys-MacBook-Pro.local>
2017-09-23 15:09:07 -0700

    Working version of script

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ function Person(age, gender, health, status){
   this.sum = function(){
     var sum = 0;
     // Age
-    sum += (-0.00000015 * Math.pow(this.age, 4)
-      + 0.00002 * Math.pow(this.age, 3)
-      - 0.00051 * Math.pow(this.age, 2)
-      - 0.6 * this.age + 5);
+    var value = Math.round((-0.00000015 * Math.pow(this.age, 4))
+      + (0.00002 * Math.pow(this.age, 3))
+      - (0.00051 * Math.pow(this.age, 2))
+      - 0.06 * this.age + 5);
+    if(value < 1)
+      value = 1;
+    sum += value;
     // Gender: 1 = male, 2 = female (default)
     if(this.gender === 1){
       sum += 0;
@@ -40,6 +43,8 @@ var laneA = [], laneB = [], car = [];
 
 function addPerson(position){
   var age = parseInt(document.getElementById('age').value);
+  if(age < 0)
+    age = 0;
   var gender = parseInt(document.getElementById('gender').value);
   var health = parseInt(document.getElementById('health').value);
   var status = parseInt(document.getElementById('status').value);
@@ -56,10 +61,10 @@ function addPerson(position){
 function chooseLane(){
   var totalA = 0, totalB = 0, totalCar = 0;
   for(var i = 0; i < laneA.length;i++){
-    totalA += lanaA[i].sum();
+    totalA += laneA[i].sum();
   }
   for(var i = 0; i < laneB.length;i++){
-    totalB += lanaB[i].sum();
+    totalB += laneB[i].sum();
   }
   for(var i = 0; i < car.length;i++){
     totalCar += car[i].sum();
